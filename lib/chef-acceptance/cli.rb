c975d596dd3f79b0daf01ef183b2baa8f3a7af9e
Patrick Wright <patrick@getchef.com>
2015-09-16 15:25:40 -0700

    destroy command and omnibus vars

--- lib/chef-acceptance/cli.rb
+++ lib/chef-acceptance/cli.rb
@@ -12,12 +12,28 @@ class ChefAcceptance
     end
 
     desc 'run TESTS', 'run acceptance tests'
+    option :omnibus_project
+    option :omnibus_version
     def test
+      env_vars = []
+      env_vars << "PROJECT_NAME=#{options[:omnibus_project]}" if options[:omnibus_project]
+      env_vars << "PROJECT_VERSION=#{options[:omnibus_version]}" if options[:omnibus_version]
       cookbooks_path = "#{File.dirname(__FILE__)}/../../cookbooks"
       cookbooks = Dir["#{cookbooks_path}/*"]
       cookbooks.each do | cookbook |
         # TODO: don't fret. this is just a quick hack
-        command = "cd #{cookbook}/acceptance; kitchen test all"
+        command = "cd #{cookbook}/acceptance; #{env_vars.join(' ')} kitchen test all"
+        system(command)
+      end
+    end
+
+    desc 'destroy INSTANCES', 'destroy instances'
+    def destroy
+      cookbooks_path = "#{File.dirname(__FILE__)}/../../cookbooks"
+      cookbooks = Dir["#{cookbooks_path}/*"]
+      cookbooks.each do | cookbook |
+        # TODO: don't fret. this is just a quick hack
+        command = "cd #{cookbook}/acceptance; kitchen destroy"
         system(command)
       end
     end
