ed3a8370c21018e6fddacb9726813e9858132b24
Patrick Wright <patrick@getchef.com>
2015-10-01 10:38:47 -0700

    cli config accessor

--- lib/chef-acceptance/cli.rb
+++ lib/chef-acceptance/cli.rb
@@ -3,15 +3,18 @@ require 'yaml'
 
 class ChefAcceptance
   class Cli < Thor
+
+    attr_accessor :config
+
     def initialize(*args)
       super
-      @root = Dir.pwd
-      @acceptance_yaml = File.join(@root, '.acceptance.yml')
-      if !File.exist?(@acceptance_yaml)
-        puts ".acceptance.yml file not found in #{@root}. Make sure you are running from an acceptance directory."
+      root = ENV['ACCEPTANCE_ROOT'] || Dir.pwd
+      acceptance_yaml = File.join(root, '.acceptance.yml')
+      if !File.exist?(acceptance_yaml)
+        puts ".acceptance.yml file not found in #{root}. Make sure you are running from an acceptance directory."
         exit 1
       end
-      @config = YAML.load_file(@acceptance_yaml)
+      @config = YAML.load_file(acceptance_yaml)
     end
 
     desc 'verify', 'Run acceptance tests'
@@ -19,9 +22,15 @@ class ChefAcceptance
     option :channel
     def verify
       env_vars = []
-      env_vars << "PROJECT_VERSION=#{@config['version']}" if options[:project_version]
-      env_vars << "CHANNEL=#{@config['channel']}" if options[:channel]
+      env_vars << "PROJECT_VERSION=#{get_option_value['version']}" if options[:project_version]
+      env_vars << "CHANNEL=#{get_option_value['channel']}" if options[:channel]
       system("#{env_vars.join(' ')} kitchen test all")
     end
+
+    no_commands {
+      def get_option_value(option)
+        config[option] || options[option]
+      end
+    } # no_commands
   end
 end
