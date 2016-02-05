f049c34581570f1d454768af1236163613e50964
John Keiser <john@johnkeiser.com>
2016-02-05 12:19:34 -0800

    Add test for .shared

--- spec/integration/chef_runner_spec.rb
+++ spec/integration/chef_runner_spec.rb
@@ -13,4 +13,26 @@ context ChefAcceptance::ChefRunner do
       expect(capture(:stdout) { runner.run! }).to match(/Running 'provision' recipe from the acceptance-cookbook in directory '.*foo'/)
     end
   end
+
+  it "calls run and picks up shared cookbooks" do
+    Dir.mktmpdir do |dir|
+      ChefAcceptance::AcceptanceCookbook.new(File.join(dir, "foo", ".acceptance")).generate
+      # Create an empty testme cookbook in .shared/testme
+      Dir.mkdir File.join(dir, ".shared")
+      Dir.mkdir File.join(dir, ".shared", "testme")
+      IO.write(File.join(dir, ".shared", "testme", "metadata.rb"), <<-EOM)
+        name "testme"
+      EOM
+      # Depend on the testme cookbook in the acceptance-cookbook
+      IO.write(File.join(dir, "foo", ".acceptance", "acceptance-cookbook", "metadata.rb"), <<-EOM)
+        name "acceptance-cookbook"
+        depends "testme"
+      EOM
+      Dir.chdir dir
+      test_suite = ChefAcceptance::TestSuite.new("foo")
+      runner = ChefAcceptance::ChefRunner.new(test_suite, "provision")
+
+      expect(capture(:stdout) { runner.run! }).to match(/Running 'provision' recipe from the acceptance-cookbook in directory '.*foo'/)
+    end
+  end
 end
